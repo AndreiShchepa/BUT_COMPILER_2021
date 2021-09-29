2abbcbf8950d084a1206d45ff5d32022540f0222
AndreiShchepa <andrewbandit228@gmail.com>
2021-09-29 23:44:52 +0200

    debug: add basic debug messages

---
+++ src/debug.h
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdio.h>
+
+#define debug_msg(...) debug_print(stdout, __VA_ARGS__)
+#define debug_msg_stderr(...) debug_print(stderr, __VA_ARGS__)
+
+#define debug_todo(...) do { \
+    fprintf(stderr, "TODO: "); \
+    debug_msg(__VA_ARGS__); \
+} while(0)
+
+#define debug_print(_dst, ...) do { \
+    fprintf((_dst), "- %s:%d in %s: ", __FILE__, __LINE__ - 1, __FUNCTION__); \
+    fprintf((_dst), __VA_ARGS__); \
+} while(0)
+
+#define debug_assert(cond) assert((cond))
