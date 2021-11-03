be04b6b8e292fef286157792832728eb2c004869
Shchepa <andrewbandit228@gmail.com>
2021-11-03 10:35:59 +0100

    expressions: small editings without functionality changes

--- src/expressions.c
+++ src/expressions.c
@@ -25,9 +25,9 @@ char Precedence_Table[][NUMBER_OF_OPERATORS] = {
         {"<<<<>>>>>>>>><><>"}, // -
 
         {"<<<<<<<>>>>>><><>"}, // ..
-        {"<<<<<<<>>>>>><><>"}, // <<
+        {"<<<<<<<>>>>>><><>"}, // <
         {"<<<<<<<>>>>>><><>"}, // <=
-        {"<<<<<<<>>>>>><><>"}, // >>
+        {"<<<<<<<>>>>>><><>"}, // >
         {"<<<<<<<>>>>>><><>"}, // >=
         {"<<<<<<<>>>>>><><>"}, // ==
         {"<<<<<<<>>>>>><><>"}, // ~=
@@ -40,6 +40,7 @@ char Precedence_Table[][NUMBER_OF_OPERATORS] = {
 
         {"<<<<<<<<<<<<<<c<c"} // $
 };
+
 char Chars[][LENGHT_OF_OPERATORS] = {
         {"#"}, {"*"}, {"/"}, {"//"},
         {"+"}, {"-"},
@@ -50,16 +51,18 @@ char Chars[][LENGHT_OF_OPERATORS] = {
         {"$"}
 };
 
-// We reversed the rules because when we copy from stack from top to bottom the expression (E) will become )E(
+// We reversed the rules because when we copy from stack
+// from top to bottom the expression (E) will become )E(
 char Rules[][LENGHT_OF_RULES] = {
         {"i"}, {")E("}, {"E+E"}, {"E-E"}, {"E*E"}, {"E/E"},
-        {"E//E"}, {"E#"}, {"E<E"}, {"E<=E"}, {"E>E"}, {"E>=E"}, {"E==E"}, {"E~=E"}, {"E..E"}
+        {"E//E"}, {"E#"}, {"E<E"}, {"E<=E"}, {"E>E"},
+        {"E>=E"}, {"E==E"}, {"E~=E"}, {"E..E"}
 };
 
 List * Init(List * list) {
     // We create the list and check for malloc
     list = malloc(sizeof(List));
-    if(list == NULL){
+    if (list == NULL) {
         return NULL;
     }
 
@@ -69,46 +72,57 @@ List * Init(List * list) {
         return NULL;
     }
 
-    // Because there is only one element, its both first and last one in the List
+    // Because there is only one element,
+    // its both first and last one in the List
     list->firstElement = TempElement;
     list->lastElement = TempElement;
 
-    // Because element is both first and last one in the List it has no previous nor next element
+    // Because element is both first and last one in the List
+    // it has no previous nor next element
     list->firstElement->previousElement = NULL;
     list->firstElement->nextElement = NULL;
 
     // We add $ as the first element of stack
-    strcpy(list->firstElement->data, "$\0");
+    strcpy(list->firstElement->data, "$");
     return list;
 }
+
 void Dispose(ElementPtr Element) {
-    if(Element == NULL){
+    if (Element == NULL) {
         return;
     }
-    // We create additional Element to help us with deletion
+
+    // We create additional Element for deleting
     ElementPtr TempElement = Element;
     ElementPtr DelElement;
+
     // Deleting
-    while(TempElement != NULL){
+    while (TempElement != NULL) {
         DelElement = TempElement;
         TempElement = TempElement->nextElement;
         free(DelElement);
     }
 }
+
 void Insert(List * list, char * data) {
-//    printf("insert: %s -> ", data);
-    if(list == NULL){
+    //printf("insert: %s -> ", data);
+    if (list == NULL) {
         return;
     }
+
     int flag = 0;
 
     ElementPtr find = list->lastElement;
-    // If stack contains for example $E and data contains +, we skip to previous element until we find one of our chars(+, -, <= etc.) and
+    // If stack contains for example $E and data contains +,
+    // we skip to previous element until we find one of
+    // our chars(+, -, <= etc.) and
     // connect the element in a way to create $<<E+,
 
-    // otherwise if there is no E on top of stack, for example $<<E+, we just copy our << with data, $<<E+ -> $<<E+<<(
+    // otherwise if there is no E on top of stack,
+    // for example $<<E+, we just copy our << with data,
+    // $<<E+ -> $<<E+<<(
     // Whether there is E on top of the stack we check with int flag
-    while((strcmp(find->data, "E") == 0) && find->previousElement != NULL){
+    while ((strcmp(find->data, "E") == 0) && find->previousElement != NULL) {
         find = find->previousElement;
         flag = 1;
     }
@@ -122,7 +136,7 @@ void Insert(List * list, char * data) {
     }
 
     // If we found expression character on stack without going through E
-    if(flag == 0){
+    if (flag == 0) {
         find->nextElement = TempElement_first;
         list->lastElement = TempElement_second;
         // We connect the chains
@@ -132,12 +146,10 @@ void Insert(List * list, char * data) {
         TempElement_first->nextElement = TempElement_second;
 
         TempElement_second->previousElement = TempElement_first;
-        TempElement_second->nextElement = NULL;
-
-        strcpy(TempElement_first->data, "<<\0");
-        strcpy(TempElement_second->data, data);
-    // If we found E on stack, means we are copying << before E and data after E
-    } else {
+        // If we found E on stack, means we are copying
+        // << before E and data after E
+    }
+    else {
         ElementPtr Element_With_E = find->nextElement;
 
         find->nextElement = TempElement_first;
@@ -152,15 +164,17 @@ void Insert(List * list, char * data) {
         Element_With_E->nextElement = TempElement_second;
 
         TempElement_second->previousElement = Element_With_E;
-        TempElement_second->nextElement = NULL;
-
-        strcpy(TempElement_first->data, "<<\0");
-        strcpy(TempElement_second->data, data);
     }
+
+    TempElement_second->nextElement = NULL;
+    strcpy(TempElement_first->data, "<<");
+    strcpy(TempElement_second->data, data);
+
     print_stack_debug(list);
 }
+
 bool Close(List * list) {
-    if(list == NULL){
+    if (list == NULL){
         return false;
     }
 
@@ -169,176 +183,235 @@ bool Close(List * list) {
     ElementPtr find = list->lastElement;
 
     // We copy into our array until we dont find closing <<
-    while((strcmp(find->data, "<<") != 0) && find->previousElement != NULL){
+    while ((strcmp(find->data, "<<") != 0) && find->previousElement != NULL) {
         strcat(Array_To_Check_Against_Rules, find->data);
         find = find->previousElement;
     }
+
     // We check against rules
-    for(int j = 0; j < 15; j++){
+    for(int j = 0; j < 15; j++) {
         // If we found correct rule
-        if(strcmp(Array_To_Check_Against_Rules, Rules[j]) == 0){
+        if (strcmp(Array_To_Check_Against_Rules, Rules[j]) == 0) {
             // We delete everything after <<
             Dispose(find->nextElement);
+
             // We change << with E
             strcpy(find->data, "E");
             find->nextElement = NULL;
             list->lastElement = find;
+
             // We were successful in finding a rule
             return true;
         }
     }
+
     // We were not successful in finding a rule
     return false;
 }
+
 void Top(List * list, char data[]) {
-    if(list == NULL){
+    if (list == NULL){
         return;
     }
+
     ElementPtr find = list->lastElement;
-    // If stack contains for example $E, we skip to previous element until we find one of our chars($, +, -, <= etc.)
-    while((strcmp(find->data, "E") == 0) && find->previousElement != NULL){
+    // If stack contains for example $E, we skip to previous element
+    // until we find one of our chars($, +, -, <= etc.)
+    while ((strcmp(find->data, "E") == 0) && find->previousElement != NULL) {
         find = find->previousElement;
     }
+
     strcpy(data, find->data);
 }
+
 void Push(List * list, char * data) {
-    if(list == NULL){
+    if (list == NULL) {
         return;
     }
+
     // We create our new element
     ElementPtr TempElement = malloc(sizeof(struct Element));
-    if(TempElement == NULL){
+    if (TempElement == NULL) {
         Deallocate(list);
         return;
     }
+
     // We find the position of last element on the stack
     ElementPtr find = list->lastElement;
-
     find->nextElement = TempElement;
+
     // Connect the chains
     TempElement->previousElement = find;
     TempElement->nextElement = NULL;
     list->lastElement = TempElement;
+
     // Copy onto the stack
     strcpy(TempElement->data, data);
 }
-void print_stack_debug(List * list){
+
+void print_stack_debug(List * list) {
     ElementPtr PrintElement = list->firstElement;
-    while(PrintElement != NULL) {
+    while (PrintElement != NULL) {
         printf("%s", PrintElement->data);
         PrintElement = PrintElement->nextElement;
     }
+
     printf("\n");
 }
-bool Check_Correct_Closure(List * list){
-    if(list != NULL){
-        // We just forcefully check if first element on stack is $ and the second one E
-        //Todo this is very obscure way to do this, also should add if statements to check if we are not dealing with NULL pointer
-        if((strcmp(list->firstElement->data, "$") == 0) && (strcmp(list->firstElement->nextElement->data, "E") == 0)){
+
+bool Check_Correct_Closure(List * list) {
+    if (list != NULL) {
+        // We just forcefully check if first element on stack
+        // is $ and the second one E
+
+        // TODO this is very obscure way to do this,
+        // also should add if statements to check if
+        // we are not dealing with NULL pointer
+        if ((strcmp(list->firstElement->data, "$") == 0) &&
+           (strcmp(list->firstElement->nextElement->data, "E") == 0))
+        {
             return true;
         }
     }
+
     return false;
 }
-int Get_Index_Of_String(char * data){
-    // if i remains 15 at the end of the for cycle, it means data contains not one of our chars (+, -, <= etc.) but a string or a variable
+
+int Get_Index_Of_String(char * data) {
+    // if i remains 15 at the end of the for cycle,
+    // it means data contains not one of our chars (+, -, <= etc.)
+    // but a string or a variable
     int i = INDEX_OF_IDENTIFICATOR;
-    for(int j = 0; j < NUMBER_OF_OPERATORS; j++){
-        if(strcmp(data, Chars[j]) == 0){
+
+    for (int j = 0; j < NUMBER_OF_OPERATORS; j++) {
+        if (strcmp(data, Chars[j]) == 0) {
             return j;
         }
     }
+
     return i;
 }
-void Deallocate(List * list){
-    if(list != NULL){
+
+void Deallocate(List * list) {
+    if (list != NULL) {
         Dispose(list->firstElement);
         free(list);
     }
 }
 
-// To get rid of test prints comment all printfs and callings of print_stack_debug(); in expression();
+// To get rid of test prints comment all printfs
+// and callings of print_stack_debug(); in expression();
 bool expression() {
-    //todo     ret = INTERNAL_ERR; pridat vsade kde moze vzniknut chyba kvoli malloc
+    // TODO ret = INTERNAL_ERR; pridat vsade kde moze vzniknut chyba kvoli malloc
     char data[3] = {"$"};
     List *list = NULL; list = Init(list);
     char precedence;
 
-    while((TOKEN_ID_EXPRESSION()) && (list != NULL)){
+    while ((TOKEN_ID_EXPRESSION()) && (list != NULL)) {
         start:
         // Look what char is on top of the stack (+, -, <= etc.)
         Top(list, data);
-        /* Takes index of a character that is in data, for example + has index of 5 due to being 5th in Chars[] array, we find that through comparing strings Chars[] and data
-         * takes index of a character that is in token, for example i has index of 15 due to being 15th in Chars[] array, we find that through comparing strings Chars[] and token.attr.id.str
-         * now we can get precedence with command Precedence_Table[5][15] and the precedence is <, because +<i in our precedence table
+
+        /* Takes index of a character that is in data,
+         * for example + has index of 5 due to being 5th in Chars[] array,
+         * we find that through comparing strings Chars[] and data
+         * takes index of a character that is in token,
+         * for example i has index of 15 due to being 15th in Chars[] array,
+         * we find that through comparing strings Chars[] and token.attr.id.str
+         * now we can get precedence with command Precedence_Table[5][15]
+         * and the precedence is <, because +<i in our precedence table
          * precedence = '<';
-        */
+         */
         precedence = Precedence_Table[Get_Index_Of_String(data)][Get_Index_Of_String(token.attr.id.str)];
         printf("top nasiel: %s oproti tokenu: %s, precedencia: %c \n", data, token.attr.id.str, precedence);
-        if(precedence == '<'){
-            // If token is a variable or a string we put i on the stack instead of copying the whole name of the variable or whole string
-            if(Get_Index_Of_String(token.attr.id.str) == INDEX_OF_IDENTIFICATOR){
+
+        if(precedence == '<') {
+            // If token is a variable or a string we put i on the stack instead
+            // of copying the whole name of the variable or whole string
+            if (Get_Index_Of_String(token.attr.id.str) == INDEX_OF_IDENTIFICATOR) {
                 strcpy(data, "i");
-            } else {
+            }
+            else {
                 // else we copy the character from the token (+, -, <= etc.)
                 strcpy(data, token.attr.id.str);
             }
-            // We copy the character from the token with << added infront of E $<<E+ or $<<E+<<( (+, -, <= etc.)
+
+            // We copy the character from the token with << added
+            // infront of E $<<E+ or $<<E+<<( (+, -, <= etc.)
             Insert(list, data);
-        } else if(precedence == '>'){
-            // We find the first << from the top of the stack to the bottom and check it against the rules, if we didnt find a rule we return false as expression is wrong
-            if(!Close(list)){
+        }
+        else if (precedence == '>') {
+            // We find the first << from the top of the stack to the bottom
+            // and check it against the rules, if we didnt find a rule
+            // we return false as expression is wrong
+            if (!Close(list)) {
                 Deallocate(list);
                 return false;
             }
+
             print_stack_debug(list);
-            // If we found a rule, we check the precedence of the new created stack by returning to the start of the while cycle
+            // If we found a rule, we check the precedence of the new created stack
+            // by returning to the start of the while cycle
             goto start;
         }
+        else if (precedence == '=') {
             // We just copy the data onto the stack
-        else if(precedence == '='){
             Push(list, token.attr.id.str);
             print_stack_debug(list);
         }
-            // Special incident with finding identificator after identificator, which means the expression has ended and we close our stack and check against the rules
-        else if(precedence == 's'){
-            while(Close(list)){
+        else if (precedence == 's') {
+            // Special incident with finding identificator after identificator,
+            // which means the expression has ended and we close our stack
+            // and check against the rules
+            while (Close(list)) {
                 print_stack_debug(list);
             }
+
             // If we were successful in reducing the expression
-            if(Check_Correct_Closure(list)){
+            if (Check_Correct_Closure(list)) {
                 printf("Vyraz je korektny.\n\n");
                 Deallocate(list);
                 return true;
             }
-            Deallocate(list);
-            return false;
-            // If we found something that doesnt have correct order, for example )(
-        } else if(precedence == 'c'){
+
             Deallocate(list);
             return false;
         }
+        else if (precedence == 'c') {
+            // If we found something that doesnt have correct order, for example )(
+            Deallocate(list);
+            return false;
+        }
+
         NEXT_TOKEN();
     }
-    // If there is internal error such as failure to allocate, list will free itself which means none of the functions will do anything and eventually we will learn
-    // that if list is empty and we never did combination of return true false with deallocation of list that there was an error along the way
-    if(list == NULL){
-        // todo ret
+
+    // If there is internal error such as failure to allocate,
+    // list will free itself which means none of the functions
+    // will do anything and eventually we will learn
+
+    // that if list is empty and we never did combination of return true
+    // false with deallocation of list that there was an error along the way
+    if (list == NULL) {
+        // TODO ret
         return INTERNAL_ERR;
     }
+
     printf("we are closing this: ");
     print_stack_debug(list);
 
     // We are reducing the expression by using our rules
-    while(Close(list)){
+    while (Close(list)) {
         print_stack_debug(list);
     }
+
     // If we were successful in reducing the expression
-    if(Check_Correct_Closure(list)){
+    if (Check_Correct_Closure(list)) {
         printf("Vyraz je korektny.\n\n");
         Deallocate(list);
         return true;
     }
+
     Deallocate(list);
     return false;
 }
--- src/expressions.h
+++ src/expressions.h
@@ -32,52 +32,55 @@ typedef struct {
 bool expression();
 
 /*
- * @brief The first initialization, we create the stack and put $ as the first element of the stack
+ * @brief The first initialization, we create the stack and
+ * put $ as the first element of the stack
  */
 List * Init(List * );
 
 /*
-* @brief We delete everything after Element with Element included
-*/
+ * @brief We delete everything after Element with Element included
+ */
 void Dispose(ElementPtr );
 
 /*
-* @brief We are inserting << with its char (+, -, <= etc.)
-*/
+ * @brief We are inserting << with its char (+, -, <= etc.)
+ */
 void Insert(List *, char *);
 
 /*
-* @brief We close the first part of the expression we find, for example <<E+<<i -> <<E+E
-*/
+ * @brief We close the first part of the expression we find,
+ * for example <<E+<<i -> <<E+E
+ */
 bool Close(List * );
 
 /*
-* @brief Returns top of the stack
-*/
+ * @brief Returns top of the stack
+ */
 void Top(List *, char * );
 
 /*
-* @brief We Copy a string on top of the stack
-*/
+ * @brief We Copy a string on top of the stack
+ */
 void Push(List *, char * );
 
 /*
-* @brief debug tool
-*/
+ * @brief debug tool
+ */
 void print_stack_debug(List * );
 
 /*
-* @brief We check if only thing left on stack is "$E"
-*/
+ * @brief We check if only thing left on stack is "$E"
+ */
 bool Check_Correct_Closure(List * );
 
 /*
-* @brief Return on which index we can find our precedence rule
-*/
+ * @brief Return on which index we can find our precedence rule
+ */
 int Get_Index_Of_String(char * );
 
 /*
-* @brief Frees list amd all its elements
-*/
+ * @brief Frees list amd all its elements
+ */
 void Deallocate(List *list);
+
 #endif // _EXPRESSION_H
