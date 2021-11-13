bc917303e78a1a2ebe604bbb05173d11ba4dc764
andrejbinovsky <binovsky.andrej@gmail.com>
2021-11-14 00:34:35 +0100

    create queue for name of identifier

---
+++ src/queue.c
@@ -0,0 +1,78 @@
+/**
+ * Project: Compiler IFJ21
+ *
+ * @file queue.h
+ *
+ * @brief Implementation of Queue
+ *
+ * @author Andrej Binovsky <xbinov00>
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "queue.h"
+
+
+Queue* queue_init(){
+    Queue *queue = calloc(1, sizeof(Queue));
+    if(!queue){
+        return NULL;
+    }
+    queue->front = NULL;
+    queue->rear = NULL;
+    return queue;
+}
+void queue_dispose(Queue *queue) {
+    QueueElementPtr *tmp;
+    while (queue->front != NULL) {
+        tmp = queue->front;
+        queue->front = queue->front->previous_element;
+        free(tmp);
+    }
+    free(queue);
+}
+
+bool queue_isEmpty(Queue *queue){
+    return (queue->front == NULL);
+}
+
+
+void queue_remove(Queue *queue){
+    if(queue_isEmpty(queue)){
+        return;
+    }
+    if(queue->front == queue->rear){
+        free(queue->front);
+        queue->front = NULL;
+        queue->rear = NULL;
+        return;
+    }
+    queue->front = queue->front->previous_element;
+    free(queue->front->next_element);
+    queue->front->next_element = NULL;
+}
+
+bool queue_add(Queue *queue, char *id){
+    QueueElementPtr *new_element =(QueueElementPtr *) calloc(1, sizeof(QueueElementPtr));
+    if(!new_element){
+        return false;
+    }
+    new_element->id = id;
+    if (queue_isEmpty(queue)) {
+        new_element->next_element = NULL;
+        queue->front = new_element;
+    } else {
+        new_element->next_element = queue->rear;;
+        queue->rear->previous_element = new_element;
+    }
+    new_element->previous_element = NULL;
+    queue->rear = new_element;
+    return true;
+}
+
+char* queue_front(Queue *queue){
+    if(queue_isEmpty(queue)){
+        return NULL;
+    }
+    return queue->front->id;
+}
---
+++ src/queue.h
@@ -0,0 +1,70 @@
+/**
+ * Project: Compiler IFJ21
+ *
+ * @file queue.h
+ *
+ * @brief Implementation of Queue
+ *
+ * @author Andrej Binovsky <xbinov00>
+ */
+
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
+#include <stdbool.h>
+
+typedef struct QueueElement{
+    char *id;
+    struct QueueElement *previous_element;
+    struct QueueElement *next_element;
+} QueueElementPtr;
+
+typedef struct queue{
+    QueueElementPtr *front;
+    QueueElementPtr *rear;
+} Queue;
+
+
+/**
+ * @brief Initialization of empty queue
+ * @return On success pointer on queue, otherwise null
+ */
+Queue* queue_init();
+
+/**
+ * @brief Free queue
+ * @param queue
+ */
+void queue_dispose(Queue *queue);
+
+
+/**
+ * @brief Function to find out if queue is empty
+ * @param queue
+ * @return When is empty True, otherwise False
+ */
+bool queue_isEmpty(Queue *queue);
+
+/**
+ * @brief Remove element from the beginning of queue
+ * @param queue
+ */
+void queue_remove(Queue *queue);
+
+/**
+ * @brief Add element from the end of queue
+ * @param queue
+ * @param id
+ * @return On success True, otherwise False
+ */
+bool queue_add(Queue *queue, char *id);
+
+/**
+ * @brief Return name of identifier
+ * @param queue
+ * @return When is empty NULL, otherwise name of identifier
+ */
+char* queue_front(Queue *queue);
+
+
+#endif // _QUEUE_H
