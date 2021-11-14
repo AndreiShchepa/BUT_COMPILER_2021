6f76e4bf249d030a747fad10331dfc3057634d64
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2021-11-14 02:34:33 +0100

    built-in functions created

--- src/code_generator.h
+++ src/code_generator.h
@@ -1,1 +1,21 @@
-void code_generator();
+
+#ifndef CODE_GENERATOR_H
+#define CODE_GENERATOR_H
+
+void code_gen_func_start();
+void code_gen_func_end();
+
+void code_gen_while_start();
+void code_gen_while_end();
+
+void code_gen_if_start();
+void code_gen_if_end();
+
+void code_gen_init_built_int();
+
+void code_gen_main_start();
+void code_gen_main_end();
+
+void code_gen();
+
+#endif // CODE_GENERATOR_H
