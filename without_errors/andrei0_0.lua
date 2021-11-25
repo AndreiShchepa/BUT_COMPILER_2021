dca7be3c917f8b6cc835c012d30dcfc2596faeca
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2021-11-25 22:57:44 +0100

    divided input tests files

--- without_errors/andrei0_0.lua
+++ without_errors/andrei0_0.lua
@@ -4,6 +4,7 @@ function main1(a, b, str, c, d)
    local id = a + c + 12 + 34
    local id1 = b + d - 90 + 78 * 54
    local id2 = str .. "string11111"
+   write(id, id1, id2)
 end
 
 main1(12, 13.5, "andr", 324, 90.899)
