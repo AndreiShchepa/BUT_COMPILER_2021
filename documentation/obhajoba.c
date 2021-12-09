7062b581d3c99ded029e15773d76e0048e1bd7e9
andrejbinovsky <binovsky.andrej@gmail.com>
2021-12-09 13:18:15 +0100

    prezetnacia wip

--- documentation/obhajoba.c
+++ documentation/obhajoba.c
@@ -1,14 +1,14 @@
 =======================OBHAJOBA PROJEKTU IFJ - 10.12.2021=======================
 •predstavenie + zadanie
 /**
- * Dobrý deň, vážená komisia, ja som Andrei a toto sú moji kolegovia, Zdenek, Richard a Andrei.
+ * Dobrý deň, vážená komisia, ja som Andrei a toto sú moji kolegovia, Zdenek, Richard a Andrej.
  * Sme tím číslo 082 a vybrali sme si variatnu zadania č. 2 - "implementace tabulky symbolu pomoci
  * tabulky s rozptylenymi polozkami".
  */
 •štruktúra projektu
 /**
  * Našu implementáciu je možné rozdeliť do 4 samostatných modulov: scanner,
- * precedenčná syntaktická analýza, parser, generátor kódu a pomocné dátové štruktúry.
+ * precedenčná syntaktická analýza, parser, generátor kódu. Tieto vsetky moduly vyuzivaju pomocné dátové štruktúry.
  * Analýzu vykonávame v jedinom priechode vstupu.
  */
 •scanner
@@ -20,6 +20,35 @@
  * 1. zda token je klicovem slovem;
  * 2. pokud jde o cislo, bude proveden prevod do odpovidajici promenne;
  */
+
+•parser
+•komunikácia so všetkým
+•zhora dole, rekurzivný zostup
+•syntaktika i sémantika
+•chyba = koniec
+/**
+ * Parser je hlavnim elementem prekladace, protoze komunikeje se vsemi jinymi elementy
+ * a ridi celou funkcnost prekladace. (Parser je spusten z mainu. - zbytecna informace, ale
+ * necham). Syntakticka analyza se provadi shora dolu metodou rekurzivniho sestupu. Kazdy nonterminal
+ * ma urcitou funkci, kde podle LL tabulky se spracovava jedno urcite pravidlo. Neni vsetko
+ * je implementovano pomoci rekurzi, protoze mi, jako vyvojaru prislo jednoduchym volat funkci v ramcich
+ * celeho pravidla.
+ *
+ * Tedka o semnatice.
+ * 1. Semanticke chyby pro nekompatibilitu typu prirazeni, navratovych hodnot funkci a predanych
+ * argumentu do funkci se detekuji pomoci 2 poli (leva strana typu a prava strana typu). Po zpracovani
+ * urciteho pravidla se provasi porovnani podle semnatiky jazyka IFJ21.
+ *
+ * 2. Kompatibilita typu ve vyrazech se detekuje stejnym zpusobam, ale je pouziity dve charove promenne.
+ *
+ * 3. Semanticke chyby pro nedefinici, redefinice a tak dal se detekuji pomoci tabulek symbolu.
+ * Pro jmena funkci mame globalni tabulku symbolu, a pro jmena promennych je vytvoren zasobnik
+ * tabulek symbolu. K takovemu reseni jsme prisli kvuli nutnosti praci s bloky a nutnost videt promenne s
+ * predchozich bloku.
+ *
+ */
+
+
 •precedenčná syntaktická analýza
     •zdola hore, výrazy
     •expression();
@@ -50,35 +79,9 @@
  * ze redukujeme zustatek a vratime rizeni. Cely text pro vsechny, aby vedeli, jak to funguje.
  * Na schuzce to probereme jeste jednou, omlouvam se za chyby)
  */
-•parser
-    •komunikácia so všetkým
-    •zhora dole, rekurzivný zostup
-    •syntaktika i sémantika
-    •chyba = koniec
-/**
- * Parser je hlavnim elementem prekladace, protoze komunikeje se vsemi jinymi elementy
- * a ridi celou funkcnost prekladace. (Parser je spusten z mainu. - zbytecna informace, ale
- * necham). Syntakticka analyza se provadi shora dolu metodou rekurzivniho sestupu. Kazdy nonterminal
- * ma urcitou funkci, kde podle LL tabulky se spracovava jedno urcite pravidlo. Neni vsetko
- * je implementovano pomoci rekurzi, protoze mi, jako vyvojaru prislo jednoduchym volat funkci v ramcich
- * celeho pravidla.
- *
- * Tedka o semnatice.
- * 1. Semanticke chyby pro nekompatibilitu typu prirazeni, navratovych hodnot funkci a predanych
- * argumentu do funkci se detekuji pomoci 2 poli (leva strana typu a prava strana typu). Po zpracovani
- * urciteho pravidla se provasi porovnani podle semnatiky jazyka IFJ21.
- *
- * 2. Kompatibilita typu ve vyrazech se detekuje stejnym zpusobam, ale je pouziity dve charove promenne.
- *
- * 3. Semanticke chyby pro nedefinici, redefinice a tak dal se detekuji pomoci tabulek symbolu.
- * Pro jmena funkci mame globalni tabulku symbolu, a pro jmena promennych je vytvoren zasobnik
- * tabulek symbolu. K takovemu reseni jsme prisli kvuli nutnosti praci s bloky a nutnost videt promenne s
- * predchozich bloku.
- *
- */
+
 
 •generátor
-    •generuje 3AC??
     •Postfix z expression
     •typy operatorov
     •tlač na konci
@@ -122,7 +125,7 @@
     •Discord
     •Schôdzky v škole - multiple for two and more hours
 /**
- * Slack za mne je pice, pokud nekoupis professional edition. (prosim nepis toto do prezentace)
+ * Slack za mne je pice (je to pravda), pokud nekoupis professional edition. (prosim nepis toto do prezentace)
  *
  * Pre komunikáciu sme používali výhradne iba discord server ktorý funguje na
  * rovnakom princípe ako slack ktorý používajú profesionálne firmy a tech.
